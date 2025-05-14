bf9199c7ef751bae3759335862fd3fe0cb744d4d
“Peio <peiohernan999@gmail.com>
2025-05-14 13:11:39 +0200

    cleaned app or smth

--- basurto/src/App.vue
+++ basurto/src/App.vue
@@ -4,15 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <h1>this is the basurto page</h1>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>This is the basurto page</h1>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
